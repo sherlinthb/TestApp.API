f10e99fe967a8733da15e8e1cffb67dbb3b00999
Sherlin Antoine <sherlin_antoine@ultimatesoftware.com>
2021-08-05 07:48:10 -0400

    Initial Commit

---
+++ Models/Info.cs
@@ -0,0 +1,10 @@
+namespace TestApp.API.Models
+{
+    public class Info
+    {
+        public int Id {get;set;}
+        public string category {get;set;}
+        public string label {get;set;}
+        public string information {get;set;} 
+    }
+}
---
+++ Models/Tip.cs
@@ -0,0 +1,10 @@
+namespace TestApp.API.Models
+{
+    public class Tip
+    {
+        public int tipId {get;set;}
+        public string tipCategory {get;set;}
+        public string tipLabel {get;set;}
+        public string tipInfo {get;set;} 
+    }
+}
